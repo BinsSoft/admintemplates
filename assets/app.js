b44233898919a4231da84db9d310eda0d9b18cfb
tonmoymatrix <tonmoy@matrixnmedia.com>
2018-07-09 14:43:38 +0530

    create new pages

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,19 @@ const scriptArr = [
 "assets/bootstrap.bundle.min.js",
 "assets/jquery.slimscroll.min.js"
 ];
-$.holdReady(true);var pageLoader;const loadScript=(index=0)=>{if(index==0){pageLoader=$("<div/>").attr({id:'page-loader'}).css({'position':'fixed','top':0,'left':0,'bottom':0,'right':0,'background':'#E6E8EC','z-index':9999}).prependTo($("body"));$("<span/>").addClass('spinner').css({'-webkit-animation':'rotation .6s infinite linear','animation':'rotation .6s infinite linear','height':'40px','width':'40px','position':'absolute','top':'50%','left':'50%','margin':'-20px 0 0 -20px','border':'2px solid rgba(0,0,0,.5)','border-top':'2px solid #fff','border-radius':'100%'}).appendTo(pageLoader)}
-$.getScript(scriptArr[index],function(){if(index==scriptArr.length-1){pageLoader.fadeOut(300);$.holdReady(false);}else{let nextIndex=index+1;loadScript(nextIndex);}})}
+$.holdReady(true);
+const loadScript = (index = 0) => {
+    
+    $.getScript(scriptArr[index], function() {
+        if (index == scriptArr.length - 1) {
+                $("#page-loader").fadeOut(300);
+                $.holdReady(false);
+        } else {
+            let nextIndex = index + 1;
+            loadScript(nextIndex);
+        }
+    })
+}
 loadScript();
 /* All plugins load successfully */
 
